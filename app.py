92a5e74d190972605774f6d37ab88b7cf9a48f54
shrey129 <shreya.sg027@gmail.com>
2026-04-20 12:32:01 +0530

    updated admin with loader

--- app.py
+++ app.py
@@ -36,6 +36,9 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login' # The name of your login route function
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
 
 
 # 3. Create Tables (The Sync)
